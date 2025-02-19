56cd93d170ebc765f6f81cca8deade1d3b511091
chandrakamalsingh007 <chandrakamalsingh007@gmail.com>
2025-02-19 13:51:06 +0545

    cart page frontend

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -19,12 +19,20 @@ const StoreContextProvider = (props) => {
         setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
     }
     
-    useEffect(()=>{
-        console.log(cartItems);
-    },[cartItems]);
+    const getTotalCartAmount = () =>{
+        let totalAmount = 0;
+        for(const item in cartItems){
+            if(cartItems[item]>0){
+                let itemInfo = food_list.find((product)=> product._id === item)
+                totalAmount += itemInfo.price*cartItems[item];
+            }
+            
+        }
+        return totalAmount;
+    }
 
     const contextValue = {
-        food_list,cartItems,setCartItems,addToCart,removefromCart
+        food_list,cartItems,setCartItems,addToCart,removefromCart,getTotalCartAmount
     }
     return(
         <StoreContext.Provider value ={contextValue}>
