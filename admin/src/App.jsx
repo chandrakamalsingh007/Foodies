d3e9d87ebd0d3cb16eada57e81ff15bebf871ecd
chandrakamalsingh007 <chandrakamalsingh007@gmail.com>
2025-02-22 14:02:54 +0545

    project completed

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -6,6 +6,7 @@ import Add from './pages/Add/Add'
 import List from './pages/List/List'
 import Orders from './pages/Orders/Orders'
 import { ToastContainer } from "react-toastify";
+import Home from './pages/Home/Home'
 
 const App = () => {
 
@@ -19,6 +20,7 @@ const App = () => {
         <div className="app-content">
           <Sidebar/>
           <Routes>
+              <Route path='/' element={<Home/>}/>
               <Route path='/add' element={<Add url={url}/>}/>
               <Route path='/list' element={<List url={url}/>}/>
               <Route path='/orders' element={<Orders url={url}/>}/>
---
+++ admin/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <h3>Welcome to the Admin Panel.</h3>
+    </div>
+  )
+}
+
+export default Home
